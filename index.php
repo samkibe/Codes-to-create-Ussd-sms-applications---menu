46ab83dbd2d1f1d54105fb3172e6f1e982b0613b
samkibe <smlkibe87@gmail.com>
2022-07-12 16:50:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// https://6ef7-102-219-208-34.in.ngrok.io/ussdsms/index.php
+
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
+
+$isRegistered = false;
+
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My Mobile number";
+
+
+?>
