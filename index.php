c76e9e792c84cdc0e6cbcb1d08a078813c2dca4f
samkibe <smlkibe87@gmail.com>
2022-07-12 17:31:29 +0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php
+<?php 
+
 // https://6ef7-102-219-208-34.in.ngrok.io/ussdsms/index.php
+
+include_once 'menu.php';
 
 // Read the variables sent via POST from our API
 $sessionId   = $_POST["sessionId"];
@@ -10,22 +13,22 @@ $text        = $_POST["text"];
 $isRegistered = false;
 
 if ($text == "" &&  !$isRegistered) {
- //user registered , string empty
+      //user registered , string empty
 
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My Mobile number";
+    //$response  = "CON What would you want to check \n";
+    //$response .= "1. My Account \n";
+    //$response .= "2. My Mobile number";
 
-}else if($text == "" && $isRegistered){
-//user unregistered , string empty
+     }else if($text == "" && $isRegistered){
+      //user unregistered , string empty
 
 
-}else if($isRegistered){
+     }else if($isRegistered){
     //user unregisered , string not empty
 
-}else{
-//user registered , string not empty
+       }else{
+     //user registered , string not empty
 
 }
 
---
+++ menu.php
@@ -0,0 +1,26 @@
+<?php
+   class menu{
+      protected $text;
+      protected $sessionId;
+
+      function __construct()
+      {
+        $this->text = $text;
+        $this->sessionId = $sessionId;
+      }
+      public function mainMenuRegistered(){}
+
+      public function mainMenuUnRegistered(){}
+
+      public function registerMenu(){}
+
+      public function sendMoneyMenu(){}
+
+      public function withdrawMoneyMenu(){}
+
+      public function checkBalanceMenu(){}
+
+   }
+   
+
+?>
