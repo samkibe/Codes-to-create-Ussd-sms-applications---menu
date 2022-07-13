4ae21f593c3562de0735838235b669ca39ca755f
samkibe <smlkibe87@gmail.com>
2022-07-13 12:06:12 +0300

    Updates

--- menu.php
+++ menu.php
@@ -1,4 +1,5 @@
 <?php
+  include_once 'util.php';
    class Menu{
       protected $text;
       protected $sessionId;
@@ -35,21 +36,49 @@
                 $pin = $textArray[2];
                  $confirmPin = $textArray[3];
                   if($pin !=$confirmPin){
-                  echo "END Your pins do not match. Please try again";
+                  echo "END OOPS Your pins do not match. Please try again";
                   }else{
               //we can register user
               //send sms
                   echo "END You have been registered";
-
              }
-
-
             }
+      }
+
+      public function sendMoneyMenu($textArray){
+        $level = count($textArray); 
+        if($level == 1){
+            echo "CON Enter mobile number of the receiver:";
+        }else if($level == 2){
+            echo "CON Enter amount:";
+        }else if($level == 3){
+            echo "CON Enter your PIN:";
+        }else if($level == 4){
+            $response = "CON Send " . $textArray[2] . " " . $textArray[1] . " \n";
+            $response.= "1. Confirm\n";
+            $response.= "2. Cancel\n";
+            $response.= Util::$GO_BACK . " Back\n";
+            $response.= Util::$GO_TO_MAIN_MENU . " Main menu\n";
+            echo $response;
+        }else if($level == 5 && $textArray[4] == 1){
+            //a confirm
+            //send the money plus process
+            //check if PIN is correct
+            //Sufficient funds
+            echo "END Your request is being processed";;
+        }else if($level == 5 && $textArray[4] == 2){
+            //cancel
+            echo "END Thank you for using our services";
+        }else if($level == 5 && $textArray[4] == Util::$GO_BACK){
+            echo "END You have requested to back up one step";
+        }else if($level == 5 && $textArray[4] == Util::$GO_TO_MAIN_MENU){
+            "END You have requested to back to main menu";
+        }else{
+            echo "END Invalid entry";
+        }
 
 
       }
-
-      public function sendMoneyMenu($textArray){}
 
       public function withdrawMoneyMenu($textArray){}
 
---
+++ util.php
@@ -0,0 +1,12 @@
+<?php
+   class Util{
+       //Db variables
+
+       //About USSD menu
+       static $GO_BACK = "98";
+       static $GO_TO_MAIN_MENU = "99";
+   }
+
+
+
+?>
