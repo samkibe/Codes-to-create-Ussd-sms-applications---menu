e09509ac9453c909490bc91c9344b5b4fba72501
Kibe <smlkibe87@gmail.com>
2022-07-14 13:05:22 +0300

    Update util.php

--- util.php
+++ util.php
@@ -2,6 +2,7 @@
    class Util{
        //Db variables
        static $DB_NAME = "ussd";
+      //It is okay to use your own values here. for user,pass and,server name
        static $DB_USER = "root";
        static $DB_USER_PASS = "";
        static $DB_SERVER_NAME = "localhost";
