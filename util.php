b39f43d6ee99e6409d9d9695be5776eb4140fc75
samkibe <smlkibe87@gmail.com>
2022-07-14 13:03:24 +0300

    Update util.php

--- util.php
+++ util.php
@@ -1,6 +1,10 @@
 <?php
    class Util{
        //Db variables
+       static $DB_NAME = "ussd";
+       static $DB_USER = "root";
+       static $DB_USER_PASS = "";
+       static $DB_SERVER_NAME = "localhost";
 
        //About USSD menu
        static $GO_BACK = "98";
