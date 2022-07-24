a4bf2522df4ee4883b3f00e0bce08e2534bb5b79
Kibe <smlkibe87@gmail.com>
2022-07-24 13:44:38 +0300

    Update util.php

--- util.php
+++ util.php
@@ -1,14 +1,14 @@
 <?php
    class Util{
        //Db variables
-       static $DB_NAME = "ussdsms";
+       static $DB_NAME = "#";
 
      //static $DB_NAME = "ussd";
       //It is okay to use your own values here. for user,pass and,server name
 
-       static $DB_USER = "root";
-       static $DB_USER_PASS = "K1b8ish??";
-       static $DB_SERVER_NAME = "localhost";
+       static $DB_USER = "#";
+       static $DB_USER_PASS = "#";
+       static $DB_SERVER_NAME = "#";
 
        //About USSD menu
        static $GO_BACK = "98";
@@ -19,12 +19,12 @@
        static $COUNTRY_CODE = "+254";
        static $TRANSACTION_FEE = 50;
 
-     //  static $API_KEY = "6eac7d474b5d0a7ea349c502a7bf841cd0b4b1a8b103bab02694aa8d5b8d6089";
-       static $API_KEY = "9dd586bf310b04ee037f5696046c2e3f7030b1c51c09761d39421d8b62cc96c1";
+     //  static $API_KEY = "#";
+       static $API_KEY = "#";
        static $API_USERNAME = "sandbox";
 
-        static $SMS_SHORTCODE = "70303";//sandbox
-        static $COMPANY_NAME = "MOBI LTD";
+        static $SMS_SHORTCODE = "#";//sandbox
+        static $COMPANY_NAME = "#";
         // static $SMS_SHORTCODE_KEYWORD = ""; //prod.
 
        //static $SMS_SHORTCODE = "70302"; //prod.
